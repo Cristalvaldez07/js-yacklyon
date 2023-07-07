e8e1339d403d1968700a05707cd8f8e1495623a8
Cristalvaldez07 <vzcristal@gmail.coml>
2023-07-06 20:21:43 -0400

    Datos

---
+++ Topics/datos.js
@@ -0,0 +1,16 @@
+// Para saber un tipo de dato
+
+var tipo = "maria";
+console.log(tipo);
+
+// Otro ejemplo
+
+var tipo;
+tipo = 123;
+console.log(typeof tipo);
+
+// ...
+
+var tipo;
+tipo = "false";
+console.log(typeof tipo);
