a7fd94acfb0af6418745e96eb23ccb136604c8cd
Cristalvaldez07 <vzcristal@gmail.coml>
2023-07-06 21:15:57 -0400

    Switch

---
+++ Topics/switch.js
@@ -0,0 +1,16 @@
+let curso = "php";
+
+switch (curso) {
+    case 'java':
+        console.log("java");
+        break;
+case 'python':
+    console.log("python3");
+    case 'javascript':
+        console.log("javascript");
+        break;
+        default:
+            console.log("no disponible");
+            break;
+
+}
