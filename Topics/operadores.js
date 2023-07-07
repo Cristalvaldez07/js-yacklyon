07cc67899d67ace30fd2db2393fd2072025a9063
Cristalvaldez07 <vzcristal@gmail.coml>
2023-07-06 20:47:50 -0400

    Operadores

---
+++ Topics/operadores.js
@@ -0,0 +1,15 @@
+// decremento 
+var num1 = 5;
+num1++;
+console.log(num1);
+
+// incremento
+var num2 = 5;
+num1--;
+console.log(num2);
+
+//residuo (se realiza igual a los demas operadores)
+
+var num3 = 18;
+console.log(num3 % 3);
+
