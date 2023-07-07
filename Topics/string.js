97081a366efaa461cc61b4b2ebb64e2ac0a63972
Cristalvaldez07 <vzcristal@gmail.coml>
2023-07-06 20:27:40 -0400

    String

---
+++ Topics/string.js
@@ -0,0 +1,29 @@
+//indexOf
+let curso = 'javascript';
+console.log(curso.indexOf('c'));
+
+//slice
+
+let curso2 = 'javascript';
+console.log(curso.slice(2,5));
+
+// replace
+
+let curso3 = 'javascript';
+console.log(curso.replace('javascript', 'php'));
+
+// split
+
+let curso4 = 'javascript';
+console.log(curso.split(''));
+
+// convertir
+
+var numero_uno = "2";
+var numero_dos = "3";
+console.log(Number(numero_uno) + Number (numero_dos));
+
+
+
+
+
