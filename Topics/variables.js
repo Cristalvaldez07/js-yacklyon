7545657c38ef3b3bc2f894345f4737895f3a8dde
Cristalvaldez07 <vzcristal@gmail.coml>
2023-07-06 20:25:45 -0400

    Variables

---
+++ Topics/variables.js
@@ -0,0 +1,16 @@
+var x = 6;
+var y = 4;
+var suma = x + y;
+console.log(x);
+
+// Otro ejemplo
+
+var nombre = "Cristal";
+var años = "18"
+var datos = ` mi nombre es ${nombre} y tengo ${años} años.}`;
+console.log(datos);
+
+// Usp de const
+
+const PI = 3.14;
+console.log(PI);
