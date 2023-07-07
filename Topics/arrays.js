dfd9279a26e327a24e56fd451474f1e9ab9a7f73
Cristalvaldez07 <vzcristal@gmail.coml>
2023-07-06 20:42:36 -0400

    Arrays

---
+++ Topics/arrays.js
@@ -0,0 +1,41 @@
+// sort
+
+var estudiantes = ["david", "beto", "ana"];
+console.log(estudiantes.sort());
+
+// push
+
+var estudiantes = ["david", "beto", "ana"];
+console.log(estudiantes.push("Luis"));
+console.log(estudiantes);
+
+// shift 
+
+var estudiantes = ["david", "beto", "ana"];
+console.log(estudiantes.push());
+console.log(estudiantes);
+
+// pop 
+
+var estudiantes = ["david", "beto", "ana"];
+console.log(estudiantes.pop());
+console.log(estudiantes);
+
+//indexOf
+
+var estudiantes = ["david", "beto", "ana"];
+console.log(estudiantes.indexOf("beto"));
+console.log(estudiantes);
+
+// unshift
+
+var estudiantes = ["david", "beto", "ana"];
+console.log(estudiantes.unshift("sara"));
+console.log(estudiantes);
+
+// concatenar
+
+var grupo_a = [1,2,3];
+var grupo_b = [4,5,6];
+console.log(grupo_a.concat(grupo_b));
+
