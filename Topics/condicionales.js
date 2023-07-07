79eb597a26cd64188a43d2ff5376bfc1eb418747
Cristalvaldez07 <vzcristal@gmail.coml>
2023-07-06 21:11:15 -0400

    Condicionales

---
+++ Topics/condicionales.js
@@ -0,0 +1,11 @@
+var a = 102;
+var b = 80;
+if (a > b) {
+    console.log(`${a} es mayor a: ${b}`);
+}
+else if(a == b) {
+    console.log("a y b son iguales")
+}
+else {
+    console.log(`${a} es menor a: ${b}`);
+}
